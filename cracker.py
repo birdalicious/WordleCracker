9655d89ba9fbc6c4d0712d4c0e3a2b88886c7714
Jack Bird <jack.bird@durham.ac.uk>
2022-01-02 02:25:32 +0000

    Made interface slightly easier to use

--- cracker.py
+++ cracker.py
@@ -114,18 +114,23 @@ class Wordle:
 
 
 wordle = Wordle(5, 'words_alpha.txt')
-print("Input \n0 for correct \n1 for incorrect \n2 for incorrect position")
+print("Input \n0 for incorrect \n1 for correct \n2 for incorrect position")
 
 while True:
     word = wordle.bestWord()
-    good = input(f'Try "{word}" ')
-    if int(good) == 0:
+    good = input(f'Does "{word}" work? Y/n ')
+    if len(good) > 0 and good.lower()[0] == 'n':
         continue
+    if len(wordle.wordChoices) <= 1:
+        print("No more word choices")
+        break
     for i, l in enumerate(word):
+        if l in wordle.correctPositions:
+            continue
         setting = int(input(f"{l}: "))
-        if setting == 1:
+        if setting == 0:
             wordle.addIncorrectLetters({l,})
-        elif setting == 0:
+        elif setting == 1:
             wordle.addCorrectPositions({l: [i]})
         elif setting == 2:
             wordle.addIncorrectPositions({l: [i]})
